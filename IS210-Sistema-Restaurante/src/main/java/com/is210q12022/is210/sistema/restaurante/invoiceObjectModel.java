e246f31e3e7bf650e357ea80c5561c9dd9cbbb45
GASC1211 <48170476+Gasc1211@users.noreply.github.com>
2022-07-08 15:54:52 -0600

    Added data object files

---
+++ IS210-Sistema-Restaurante/src/main/java/com/is210q12022/is210/sistema/restaurante/invoiceObjectModel.java
@@ -0,0 +1,47 @@
+package com.is210q12022.is210.sistema.restaurante;
+
+import java.util.Date;
+
+public class invoiceObjectModel {
+    private int invoiceId;
+    private Date datetime;
+    private float subTotal;
+    private float taxes;
+    private float total;
+
+    public int getInvoiceId() {
+        return invoiceId;
+    }
+
+    public Date getDatetime() {
+        return datetime;
+    }
+
+    public void setDatetime(Date datetime) {
+        this.datetime = datetime;
+    }
+
+    public float getSubTotal() {
+        return subTotal;
+    }
+
+    public void setSubTotal(float subTotal) {
+        this.subTotal = subTotal;
+    }
+
+    public float getTaxes() {
+        return taxes;
+    }
+
+    public void setTaxes(float taxes) {
+        this.taxes = taxes;
+    }
+
+    public float getTotal() {
+        return total;
+    }
+
+    public void setTotal(float total) {
+        this.total = total;
+    }
+}
---
+++ IS210-Sistema-Restaurante/src/main/java/com/is210q12022/is210/sistema/restaurante/userObjectModel.java
@@ -0,0 +1,37 @@
+package com.is210q12022.is210.sistema.restaurante;
+
+public class userObjectModel {
+
+    private int userId;
+    private String username;
+    private String password;
+    private String userType;
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUserType() {
+        return userType;
+    }
+
+    public void setUserType(String userType) {
+        this.userType = userType;
+    }
+}
