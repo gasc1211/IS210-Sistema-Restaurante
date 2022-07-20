64c95656f72f1ba7ded62438e7887e0fc1f96caa
cesar17maldonado <cesarmolina1709@gmail.com>
2022-07-20 17:36:59 -0600

    Limpiar tablas

--- IS210-Sistema-Restaurante/src/main/java/com/is210q12022/is210/sistema/restaurante/Cocina.java
+++ IS210-Sistema-Restaurante/src/main/java/com/is210q12022/is210/sistema/restaurante/Cocina.java
@@ -10,9 +10,8 @@ import javax.swing.table.DefaultTableModel;
 
 
 public class Cocina extends javax.swing.JFrame {
-
-    DefaultTableModel model1;
     DefaultTableModel model2;
+    DefaultTableModel model1;
     
     public Connection Conectar() {
         Connection con = null;
@@ -32,6 +31,7 @@ public class Cocina extends javax.swing.JFrame {
     }
 
     public void Cargar() {
+        
         Connection con1 = null;
         PreparedStatement pst = null;
         ResultSet rs = null;
@@ -62,14 +62,15 @@ public class Cocina extends javax.swing.JFrame {
     }
 
     public void limpiar1() {
-            for (int i = 0; i <= jTable1.getRowCount(); i++) {
+       for (int i = 0; i < jTable1.getRowCount(); i++) {
             model1.removeRow(i);
             i = i - 1;
         }
+            
     }
     
         public void limpiar2() {
-            for (int i = 0; i <= jTable2.getRowCount(); i++) {
+            for (int i = 0; i < jTable2.getRowCount(); i++) {
             model2.removeRow(i);
             i = i - 1;
         }
@@ -88,8 +89,6 @@ public class Cocina extends javax.swing.JFrame {
         jButton2 = new javax.swing.JButton();
         jTextField1 = new javax.swing.JTextField();
         jButton3 = new javax.swing.JButton();
-        jButton4 = new javax.swing.JButton();
-        jButton5 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
@@ -164,22 +163,6 @@ public class Cocina extends javax.swing.JFrame {
         });
         getContentPane().add(jButton3, new org.netbeans.lib.awtextra.AbsoluteConstraints(270, 530, -1, -1));
 
-        jButton4.setText("jButton4");
-        jButton4.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton4ActionPerformed(evt);
-            }
-        });
-        getContentPane().add(jButton4, new org.netbeans.lib.awtextra.AbsoluteConstraints(490, 520, -1, -1));
-
-        jButton5.setText("jButton5");
-        jButton5.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton5ActionPerformed(evt);
-            }
-        });
-        getContentPane().add(jButton5, new org.netbeans.lib.awtextra.AbsoluteConstraints(490, 550, -1, -1));
-
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
@@ -194,7 +177,8 @@ public class Cocina extends javax.swing.JFrame {
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-       Cargar();
+       limpiar1();
+        Cargar();
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jTable1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jTable1MouseClicked
@@ -202,7 +186,7 @@ public class Cocina extends javax.swing.JFrame {
     }//GEN-LAST:event_jTable1MouseClicked
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
-        
+        limpiar2();
         Connection con1 = null;
         PreparedStatement pst = null;
         ResultSet rs = null;
@@ -233,14 +217,6 @@ public class Cocina extends javax.swing.JFrame {
             
             }  
     }//GEN-LAST:event_jButton3ActionPerformed
-
-    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
-        limpiar1();
-    }//GEN-LAST:event_jButton4ActionPerformed
-
-    private void jButton5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton5ActionPerformed
-        limpiar2();
-    }//GEN-LAST:event_jButton5ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -281,8 +257,6 @@ public class Cocina extends javax.swing.JFrame {
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
     private javax.swing.JButton jButton3;
-    private javax.swing.JButton jButton4;
-    private javax.swing.JButton jButton5;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JTable jTable1;
