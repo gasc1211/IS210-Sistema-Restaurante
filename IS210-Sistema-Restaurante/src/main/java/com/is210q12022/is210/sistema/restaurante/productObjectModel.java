ef8bb57c7ce6962192190763ef2be0d59123ce02
zzMAZzz <mzelayaf@unah.hn>
2022-07-24 14:59:45 -0600

    Revert "Update 5 - Factura"
    This reverts commit 7a7a67a91572e3bfe95288461e919f7ecd0f4d41.

--- IS210-Sistema-Restaurante/src/main/java/com/is210q12022/is210/sistema/restaurante/productObjectModel.java
+++ IS210-Sistema-Restaurante/src/main/java/com/is210q12022/is210/sistema/restaurante/productObjectModel.java
@@ -1,66 +1,41 @@
 package com.is210q12022.is210.sistema.restaurante;
 
 public class productObjectModel {
-    private int orderId;
-    private String bebida;
-    private float precioB;
-    private int cantidadB;
-    private String comida;
-    private float precioC;
-    private int cantidadC;
+    private int productId;
+    private String productName;
+    private float price;
+    private int inventory;
 
-    public int getorderId() {
-        return orderId;
+    public int getProductId() {
+        return productId;
     }
 
-    public void setorderId(int orderId) {
-        this.orderId = orderId;
+    public void setProductId(int productId) {
+        this.productId = productId;
     }
 
-    public String getBebida() {
-        return bebida;
+    public String getProductName() {
+        return productName;
     }
 
-    public void setBebida(String bebida) {
-        this.bebida = bebida;
+    public void setProductName(String productName) {
+        this.productName = productName;
     }
 
-    public float getPriceb() {
-        return precioB;
+    public float getPrice() {
+        return price;
     }
 
-    public void setPriceb(float precioB) {
-        this.precioB = precioB;
+    public void setPrice(float price) {
+        this.price = price;
     }
 
-    public int getCantb() {
-        return cantidadB;
+    public int getInventory() {
+        return inventory;
     }
 
-    public void setCantb(int cantB) {
-        this.cantidadB = cantB;
-    }
-    public String getComida() {
-        return comida;
+    public void setInventory(int inventory) {
+        this.inventory = inventory;
     }
 
-    public void setComida(String comida) {
-        this.comida = comida;
-    }
-
-    public float getPricec() {
-        return precioC;
-    }
-
-    public void setPricec(float precioC) {
-        this.precioC = precioC;
-    }
-
-    public int getCantc() {
-        return cantidadC;
-    }
-
-    public void setCantc(int cantidadC) {
-        this.cantidadC = cantidadC;
-    }
 }
